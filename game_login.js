3ccad87437064cdced3946656e3aebd41d720ea5
sraya691 <98817930+sraya691@users.noreply.github.com>
2022-07-04 17:39:37 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,8 +5,8 @@ function addUser() {
 
     //Set "player1_name" using localStorage.setItem() function
 	  //Set "player2_name" using localStorage.setItem() function
-    localStorage.setItem("player1_name");
-    localStorage.setItem("player2_name");
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
     window.location = "game_page.html";
 }
 
